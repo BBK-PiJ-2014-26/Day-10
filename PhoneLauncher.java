b75f334c23002ab0ff8cbef53155a89a13c53f7e
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-12-27 21:12:35 +0000

    solved

--- PhoneLauncher.java
+++ PhoneLauncher.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class PhoneLauncher {
+	private SmartPhone myPhone;
+
+	public PhoneLauncher() {
+		this.myPhone = new SmartPhone();
+	}
 
 	public static void main(String[] args) {
 
@@ -9,7 +14,6 @@ public class PhoneLauncher {
 	}
 
 	public void launch() {
-		SmartPhone myPhone = new SmartPhone();
 		System.out.println("\n" + "Please select an operation. \n" + "1 to call.\n" + "2 to display the last 10 numbers called.\n" + "3 to browse the web.\n" + "4 for your GPS position.\n" + "5 to initiate an alarm.\n" + "6 to play a game.\n" + "7 to quit.\n");
 		Scanner sc = new Scanner(System.in);
 		boolean finished = false;
@@ -19,37 +23,37 @@ public class PhoneLauncher {
 					System.out.print("Enter a phone number: ");
 					sc = new Scanner(System.in);
 					String number = sc.next();
-					myPhone.call(number);
-					myPhone.addToLastNumbers(number);
+					this.myPhone.call(number);
+					this.myPhone.addToLastNumbers(number);
 					this.launch();
-	
+
 			} else if (choice == 2) {
-					myPhone.printLastNumbers();
+					this.myPhone.printLastNumbers();
 					this.launch();
-	
-			} else if (choice == 3) { 
+
+			} else if (choice == 3) {
 					System.out.print("Enter a web address: ");
 					sc = new Scanner(System.in);
 					String address = sc.next();
-					myPhone.browseWeb(address);
+					this.myPhone.browseWeb(address);
 					this.launch();
 
 			} else if (choice == 4) {
-					System.out.println("Your position is: " + myPhone.findPosition());
+					System.out.println("Your position is: " + this.myPhone.findPosition());
 					this.launch();
-		
+
 			} else if (choice == 5) {
 					System.out.print("Enter your alarm message: ");
 					sc = new Scanner(System.in);
 					String message = sc.next();
-					myPhone.ringAlarm(message);
+					this.myPhone.ringAlarm(message);
 					this.launch();
 
 			} else if (choice == 6) {
 					System.out.print("Enter the name of the game: ");
 					sc = new Scanner(System.in);
 					String nameOfGame = sc.next();
-					myPhone.playGame(nameOfGame);
+					this.myPhone.playGame(nameOfGame);
 					this.launch();
 
 			} else if (choice == 7) {
@@ -64,6 +68,6 @@ public class PhoneLauncher {
 			System.out.print("Invalid option. Please try again.");
 			this.launch();
 		}
-		
+
 	}
 }
